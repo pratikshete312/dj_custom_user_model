2fb33664fec65f6bb2f086bea9a458cab9f8aae7
Pratik312-gif <pratikshete82@gmail.com>
2021-07-08 11:42:02 +0530

    first commit

---
+++ accounts/views.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render, redirect
+
+from accounts.forms import UserAdminCreationForm
+
+
+def register(req):
+    form = UserAdminCreationForm()
+    if req.method == 'POST':
+        form = UserAdminCreationForm(req.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('register')
+    return render(req, 'register.html', {'form': form})
